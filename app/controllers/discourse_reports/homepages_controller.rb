bbae20718e272c67ab860c3a809e15a3bb8fb94a
Arthur Zaharov <arthur.zaharov@flatstack.com>
2015-02-09 17:28:18 +0300

    Change top topics on hmepage|

--- app/controllers/discourse_reports/homepages_controller.rb
+++ app/controllers/discourse_reports/homepages_controller.rb
@@ -3,7 +3,7 @@ module DiscourseReports
     skip_before_filter :check_xhr
 
     def show
-      serialized = serialize_data(topics, TopicHomepageSerializer, scope: guardian)
+      serialized = serialize_data(topics, TopicHomepageSerializer)
 
       respond_to do |format|
         format.html do
@@ -18,9 +18,9 @@ module DiscourseReports
     private
 
     def topics
-      Topic.includes(:category).top_viewed(3).where(
+      Topic.joins(:top_topic).where("top_topics.yearly_score > 0").includes(:category).where(
         categories: { name: SiteSetting.parent_categories.split('|') }
-      )
+      ).order(TopicQuerySQL.order_top_for('yearly_score')).limit(15)
     end
   end
 end
