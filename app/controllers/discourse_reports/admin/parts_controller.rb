1efef2805e8bf99a0048c1a5fd4a16a24e6db584
VladimirMikhailov <vladimir.mikhailov@flatstack.com>
2015-02-17 15:34:50 +0300

    Use admin module in scope of discourse reports instead of referring to the root Admin - prevents circular dependencty autoloading

--- app/controllers/discourse_reports/admin/parts_controller.rb
+++ app/controllers/discourse_reports/admin/parts_controller.rb
@@ -1,44 +1,46 @@
 module DiscourseReports
-  class Admin::PartsController < ::Admin::AdminController
-    after_action :update_part_constraints, only: %i(create update destroy)
+  module Admin
+    class PartsController < ::Admin::AdminController
+      after_action :update_part_constraints, only: %i(create update destroy)
 
-    def index
-      parts = Part.order('position').includes(:chapters).all
-      render_serialized(parts, PartSerializer)
-    end
-
-    def create
-      part = Part.new(part_params)
-      if part.save
-        render_serialized(part, PartSerializer)
-      else
-        render_json_error(part)
+      def index
+        parts = Part.order('position').includes(:chapters).all
+        render_serialized(parts, PartSerializer)
       end
-    end
 
-    def update
-      part = Part.where(id: params.require(:id)).first
-      if part.update(part_params)
-        render_serialized(part, PartSerializer)
-      else
-        render_json_error(part)
+      def create
+        part = Part.new(part_params)
+        if part.save
+          render_serialized(part, PartSerializer)
+        else
+          render_json_error(part)
+        end
       end
-    end
 
-    def destroy
-      part = Part.where(id: params.require(:id)).first
-      part.destroy if part.present?
-      render nothing: true
-    end
+      def update
+        part = Part.where(id: params.require(:id)).first
+        if part.update(part_params)
+          render_serialized(part, PartSerializer)
+        else
+          render_json_error(part)
+        end
+      end
 
-    private
+      def destroy
+        part = Part.where(id: params.require(:id)).first
+        part.destroy if part.present?
+        render nothing: true
+      end
 
-    def part_params
-      params.require(:part).permit(:name, :position, :description)
-    end
+      private
 
-    def update_part_constraints
-      Part.update_part_constraints
+      def part_params
+        params.require(:part).permit(:name, :position, :description)
+      end
+
+      def update_part_constraints
+        Part.update_part_constraints
+      end
     end
   end
 end
