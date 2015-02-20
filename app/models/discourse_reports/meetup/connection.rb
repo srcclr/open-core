6358e4996efeb45ec6ba97660978173b4af8983d
VladimirMikhailov <vladimir.mikhailov@flatstack.com>
2015-02-20 18:56:15 +0300

    Use rails cache for faraday-http-cache

--- app/models/discourse_reports/meetup/connection.rb
+++ app/models/discourse_reports/meetup/connection.rb
@@ -8,7 +8,7 @@ module DiscourseReports
 
       def initialize
         @connection ||= Faraday.new(url: self.class.site) do |builder|
-          builder.use :http_cache, store: store, serializer: Marshal
+          builder.use :http_cache, store: Rails.cache, serializer: Marshal
           builder.adapter :net_http
         end
       end
@@ -16,12 +16,6 @@ module DiscourseReports
       def get(*args)
         result = connection.get(*args)
         result.success? ? result : ErrorResponse.new(result.body)
-      end
-
-      private
-
-      def store
-        @store ||= ActiveSupport::Cache.lookup_store(:redis_store)
       end
     end
   end
