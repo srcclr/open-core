a630a3a2fd70e688778e8dfd52510cd7c8dc19d3
Arthur Zaharov <arthur.zaharov@flatstack.com>
2015-04-13 13:27:42 +0300

    Add additional author blog topics

--- app/serializers/discourse_reports/extended_user_serializer.rb
+++ app/serializers/discourse_reports/extended_user_serializer.rb
@@ -2,8 +2,14 @@ module DiscourseReports
   class ExtendedUserSerializer < BasicUserSerializer
     attributes :company
 
+    has_many :additional_blogs, serializer: BasicTopicSerializer, embed: :objects
+
     def company
       object.user_fields[UserField.find_by(name: 'Company').id.to_s]
+    end
+
+    def additional_blogs
+      object.topics.by_newest.where(archetype: 'blog').first(5)
     end
   end
 end
