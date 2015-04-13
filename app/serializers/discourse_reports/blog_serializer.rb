66359c991765b59a1210e40295be71ecfe1d80c5
Arthur Zaharov <arthur.zaharov@flatstack.com>
2015-04-13 14:49:42 +0300

    Cut long topic body in blogs list page

--- app/serializers/discourse_reports/blog_serializer.rb
+++ app/serializers/discourse_reports/blog_serializer.rb
@@ -1,9 +1,5 @@
 module DiscourseReports
   class BlogSerializer < TopicListItemSerializer
-    has_one :first_post, serializer: BasicPostSerializer, embed: :objects
-
-    def first_post
-      object.posts[0]
-    end
+    has_one :first_post, serializer: AdminPostSerializer, embed: :objects
   end
 end
