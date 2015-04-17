037e1efd6905b163ca70f62568493a5f511cbcab
Arthur Zaharov <arthur.zaharov@flatstack.com>
2015-04-17 14:59:55 +0300

    Get actual username and user company

--- app/serializers/topic_view_serializer.rb
+++ app/serializers/topic_view_serializer.rb
@@ -2,10 +2,19 @@ TopicViewSerializer.class_eval do
   attributes_from_topic :chapter_id, :position
 
   has_one :user, serializer: DiscourseReports::ExtendedUserSerializer, embed: :object
+  has_many :additional_topics_by_user, serializer: BasicTopicSerializer, embed: :objects
 
   private
 
+  def archetype
+    object.topic.archetype
+  end
+
   def user
-    object.topic.user if object.topic.archetype.eql?('blog')
+    object.topic.user
+  end
+
+  def additional_topics_by_user
+    user.topics.where(archetype: archetype).order('RANDOM()').first(5)
   end
 end
