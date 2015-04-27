85df3ee81628e054ddb9aa200eb243c65c99c04a
Arthur Zaharov <arthur.zaharov@flatstack.com>
2015-04-27 18:45:29 +0300

    Fix categories names on homepage bug

--- app/serializers/discourse_reports/topic_homepage_serializer.rb
+++ app/serializers/discourse_reports/topic_homepage_serializer.rb
@@ -1,5 +1,5 @@
 module DiscourseReports
   class TopicHomepageSerializer < TopicListItemSerializer
-    has_one :category, serializer: CategoryDetailedSerializer
+    has_one :category, serializer: CategoryDetailedSerializer, embed: :objects
   end
 end
