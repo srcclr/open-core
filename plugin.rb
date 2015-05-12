1af8c973d0a8893edf21d0535ef7ffdc5ebff448
Arthur Zaharov <arthur.zaharov@flatstack.com>
2015-05-12 18:56:44 +0300

    Add ability to search topic by tags

--- plugin.rb
+++ plugin.rb
@@ -210,6 +210,7 @@ def initialize_additional_libs
   require(File.expand_path('../lib/post_revisor', __FILE__))
   require(File.expand_path('../lib/preload_parts', __FILE__))
   require(File.expand_path('../lib/suggested_topics_builder', __FILE__))
+  require(File.expand_path('../lib/search', __FILE__))
   require(File.expand_path('../app/serializers/topic_view_serializer', __FILE__))
   require(File.expand_path('../app/serializers/site_serializer', __FILE__))
   require(File.expand_path('../app/serializers/current_user_serializer', __FILE__))
