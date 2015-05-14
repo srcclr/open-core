0e53131c2ae6d448a69d0679d0f4f8daafc5901e
Askar Zinurov <askar.zinurov@flatstack.com>
2015-05-15 00:48:09 +0300

    Fix topic guardian

--- lib/guardian.rb
+++ lib/guardian.rb
@@ -1,5 +1,7 @@
 Guardian.class_eval do
-  define_method("can_see_topic?") do |topic|
-    [SiteSetting.guidelines_topic_id].include? topic.id
+  define_method("can_see_topic_with_publicity?") do |topic|
+    [SiteSetting.guidelines_topic_id].include?(topic.id) ||
+      can_see_topic_without_publicity?(topic)
   end
+  alias_method_chain :can_see_topic?, :publicity
 end
