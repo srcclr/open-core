2111618dfbea1afd2f1cab275049491d044e5008
VladimirMikhailov <vladimir.mikhailov@flatstack.com>
2015-05-28 15:15:46 +0300

    Redirect for unauthorized users if login require from book and about pages

--- app/controllers/discourse_reports/about_site_controller.rb
+++ app/controllers/discourse_reports/about_site_controller.rb
@@ -1,5 +1,7 @@
 module DiscourseReports
   class AboutSiteController < StaticController
+    before_filter :redirect_to_login_if_required
+
     def show
       @topic_view = TopicView.new(SiteSetting.guidelines_topic_id, current_user)
       respond_to do |format|
