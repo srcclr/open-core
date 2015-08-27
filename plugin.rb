55e1c7e2ce5411909431aca27144d3d5d5db198f
Aleksey Yeremeyev <aleks.yeremeyev@flatstack.com>
2015-08-27 18:45:07 +0300

    Style cleanup

--- plugin.rb
+++ plugin.rb
@@ -14,10 +14,7 @@ require(File.expand_path('../lib/discourse_reports', __FILE__))
 
 register_asset('stylesheets/admin/settings.css.scss')
 
-register_asset('stylesheets/base/framework.css.scss')
 register_asset('stylesheets/base/base.css.scss')
-register_asset('stylesheets/base/form.css.scss')
-register_asset('stylesheets/base/grid.css.scss')
 
 register_asset('stylesheets/components/comments.css.scss')
 register_asset('stylesheets/components/sections.css.scss')
