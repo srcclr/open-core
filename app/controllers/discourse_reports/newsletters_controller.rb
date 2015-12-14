a19b6b68e1336af649a4f7e5b5bf2f123015bfff
ArthurZaharov <arthur.zaharov@flatstack.com>
2015-12-14 14:44:26 +0300

    Add title and back button to newsletter page

--- app/controllers/discourse_reports/newsletters_controller.rb
+++ app/controllers/discourse_reports/newsletters_controller.rb
@@ -3,13 +3,15 @@ module DiscourseReports
     skip_before_filter :check_xhr, :redirect_to_login_if_required
 
     def show
+      serialized = serialize_data(newsletter, NewsletterSerializer, root: false)
+
       respond_to do |format|
         format.html do
-          store_preloaded('newsletter',  MultiJson.dump(body: newsletter.body))
+          store_preloaded('newsletter',  MultiJson.dump(serialized))
           render 'default/empty'
         end
 
-        format.json { render_json_dump(body: newsletter.body) }
+        format.json { render_json_dump(serialized) }
       end
     end
 
