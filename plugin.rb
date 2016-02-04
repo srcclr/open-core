1a0c033d82eaa5bff293351818d51dbfc9049019
Aleksey Yeremeyev <aleks.yeremeyev@flatstack.com>
2016-02-04 13:50:19 +0300

    Improve homepage and Projects pages layout
    * discussions list
    * projects icons

--- plugin.rb
+++ plugin.rb
@@ -58,6 +58,7 @@ register_asset('stylesheets/views/topic-post.css.scss')
 register_asset('stylesheets/views/newsletter.css.scss')
 register_asset('stylesheets/views/error.css.scss')
 register_asset('stylesheets/views/topic-list.css.scss')
+register_asset('stylesheets/views/projects.css.scss')
 register_asset('stylesheets/vendor/bootstrap-datepicker.css.scss')
 
 register_asset('javascripts/vendor/bootstrap-datepicker.js')
